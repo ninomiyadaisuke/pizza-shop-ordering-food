a2f4683c59a5890ccb55045c5a5cc6628694a7aa
二ノ宮　大翼 <ninomiyadaisuke@ninomiyataiyokunoMacBook-Pro.local>
2020-12-11 09:58:08 +0900

    Add Authentication

--- src/App.js
+++ src/App.js
@@ -7,19 +7,20 @@ import { FoodDialog } from "./FoodDialog/FoodDialog"
 import { Order } from "./Order/Order"
 import { useOpenFood } from "./Hooks/useOpenFood"
 import { useOrders } from "./Hooks/useOrders"
-import {useTitle} from "./Hooks/useTitle"
-
+import { useTitle } from "./Hooks/useTitle"
+import { useAuthentication } from "./Hooks/useAuthentication"
 
 function App() {
   const openFood = useOpenFood()
   const orders = useOrders()
+  const auth = useAuthentication()
   useTitle({...openFood, ...orders})
   return (
     <>
       <GlobalStyle />
       <FoodDialog  {...openFood} {...orders}/>
-      <Navbar />
-      <Order {...orders} {...openFood}/>
+      <Navbar {...auth}/>
+      <Order {...orders} {...openFood} {...auth}/>
       <Banner />
       <Menu {...openFood}/>
       <div>Hello SliceLine</div>
